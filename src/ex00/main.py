76b87ac45d92f2fcba1efa64ab96a517204f66a9
Ohaggard <marlausor@gmail.com>
2023-10-24 02:18:55 +0300

    ex00 excess information deleted

--- src/ex00/main.py
+++ src/ex00/main.py
@@ -4,7 +4,6 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("line_count", help = "Enter strings count", type = int)
     args = parser.parse_args()
-    print(f"first arg is {args.line_count}")
     strscount = args.line_count
 
     for i in range(strscount):
