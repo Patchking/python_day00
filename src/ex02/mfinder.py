ac1d5f540d3502abda21811dec6144faa849e2f9
Ohaggard <marlausor@gmail.com>
2023-10-24 15:52:52 +0300

    mfinder now print Error when input string is longer then it needs

--- src/ex02/mfinder.py
+++ src/ex02/mfinder.py
@@ -1,6 +1,14 @@
 from argparse import ArgumentParser
 
-def compare_strs(orig_strs, input_strs) -> bool:
+def is_input_correct(input_strs, orig_size):
+    for i in range(orig_size[1]):
+        if len(input_strs[i]) != orig_size[0]:
+            return False
+    return True
+
+def compare_strs(orig_strs, input_strs, orig_size) -> bool:
+    if not is_input_correct(input_strs, orig_size):
+        raise IndexError
     for i in range(3):
         for j in range(5):
             if orig_strs[i][j] == "*" and input_strs[i][j] == "*":
@@ -17,12 +25,13 @@ def main():
                  "**#**",
                  "*#*#*"]
     input_strs = []
+    orig_size = (len(orig_strs[0]), len(orig_strs))
     for i in range(3):
         input_strs.append(input())
 
     is_equal = False
     try:
-        is_equal = compare_strs(orig_strs, input_strs)
+        is_equal = compare_strs(orig_strs, input_strs, orig_size)
     except IndexError:
         print("Error")
         return
